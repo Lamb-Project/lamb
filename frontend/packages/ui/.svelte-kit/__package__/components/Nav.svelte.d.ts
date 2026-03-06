a876d7e44b96cd2582fc08655ef8becc0681b911
Adrián Ferrer Gutiérrez <adrian.ferrer.gutierrez@estudiantat.upc.edu>
2026-03-06 12:22:09 +0100

    fix: remove pnpm store and svelte-kit generated files from tracking

--- frontend/packages/ui/.svelte-kit/__package__/components/Nav.svelte.d.ts
+++
@@ -1,6 +0,0 @@
-export default Nav;
-type Nav = {
-    $on?(type: string, callback: (e: any) => void): () => void;
-    $set?(props: Partial<Record<string, never>>): void;
-};
-declare const Nav: import("svelte").Component<Record<string, never>, {}, "">;
